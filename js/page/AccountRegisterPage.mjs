2a4fb205e9316a731fb2b3c22a865547a9ee02fc
SindreVonH <149319539+SindreVonH@users.noreply.github.com>
2024-05-28 21:11:53 +0200

    Update AccountRegisterPage.mjs

--- js/page/AccountRegisterPage.mjs
+++ js/page/AccountRegisterPage.mjs
@@ -1,8 +1,8 @@
-import { registerUser } from '/Exam-project/pageAPI/fetchregister.mjs';
-import { addHeader } from '/Exam-project/pageElementes/header.mjs';
-import { addFooter } from '/Exam-project/pageElementes/footer.mjs';
-import { clearAndLoadCSS} from '/Exam-project/pageElementes/stylesManager.mjs';
-import { loadCSS} from '/Exam-project/pageElementes/stylesLoader.mjs';
+import { registerUser } from '../pageAPI/fetchregister.mjs';
+import { addHeader } from '../pageElementes/header.mjs';
+import { addFooter } from '../pageElementes/footer.mjs';
+import { clearAndLoadCSS} from '../pageElementes/stylesManager.mjs';
+import { loadCSS} from '../pageElementes/stylesLoader.mjs';
 
 //Function to create register page
 export const displayRegisterPage = async () => {
