6f4c302c3e0acabf51c9b56ea54873832f814193
SindreVonH <149319539+SindreVonH@users.noreply.github.com>
2024-05-28 21:01:17 +0200

    Update BlogPostEditPage.mjs

--- js/page/BlogPostEditPage.mjs
+++ js/page/BlogPostEditPage.mjs
@@ -1,8 +1,8 @@
-import { getSinglePost, updatePost } from '../pageAPI/fetchUpdate.mjs';
-import { addHeader } from '../pageElementes/header.mjs';
-import { addFooter } from '../pageElementes/footer.mjs';
-import { clearAndLoadCSS} from '../pageElementes/stylesManager.mjs';
-import { loadCSS} from '../pageElementes/stylesLoader.mjs';
+import { getSinglePost, updatePost } from '/Exam-project/pageAPI/fetchUpdate.mjs';
+import { addHeader } from '/Exam-project/pageElementes/header.mjs';
+import { addFooter } from '/Exam-project/pageElementes/footer.mjs';
+import { clearAndLoadCSS} from '/Exam-project/pageElementes/stylesManager.mjs';
+import { loadCSS} from '/Exam-project/pageElementes/stylesLoader.mjs';
 
 //Function to create postEditPage
 export const displayEditPostPage = async () => {
