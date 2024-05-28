6b90f6bd0c6605202254cd81b4d5e14e08dd1325
SindreVonH <149319539+SindreVonH@users.noreply.github.com>
2024-05-28 21:12:17 +0200

    Update AccountLoginPage.mjs

--- js/page/AccountLoginPage.mjs
+++ js/page/AccountLoginPage.mjs
@@ -1,8 +1,8 @@
-import { loginUser } from '/Exam-project/pageAPI/fetchlogin.mjs';
-import { addHeader } from '/Exam-project/pageElementes/header.mjs';
-import { addFooter } from '/Exam-project/pageElementes/footer.mjs';
-import { clearAndLoadCSS} from '/Exam-project/pageElementes/stylesManager.mjs';
-import { loadCSS} from '/Exam-project/pageElementes/stylesLoader.mjs';
+import { loginUser } from '../pageAPI/fetchlogin.mjs';
+import { addHeader } from '../pageElementes/header.mjs';
+import { addFooter } from '../pageElementes/footer.mjs';
+import { clearAndLoadCSS} from '../pageElementes/stylesManager.mjs';
+import { loadCSS} from '../pageElementes/stylesLoader.mjs';
 
 //Function to create login page
 export const displayLoginPage = async () => {
